2bb7ad41dd4e953a716e628f2ba2123ee992991a
milliondegree <zhangjiachi1007@gmail.com>
2018-11-01 00:32:08 -0400

    hist

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 SAVE_PATH = './plot/'
 
@@ -16,12 +17,20 @@ def getArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', type=str, default='./dataset/crime2017.csv', help='the file path')
     parser.add_argument('--hvar', nargs='+', help="attributes' names to draw histgrams")
+    parser.add_argument('--bins', nargs='+', help="bin number list with respect to values in hvar")
     parser.add_argument('--qvar', type=str, nargs='+', help="attributes' names to draw correlation graphs");
     return parser.parse_args()
 
 
-def plotHist(myData, attribute_list, save_path):
-    pass
+def plotHist(myData, attribute_list, bin_num, save_path):
+    for a, bn in zip(attribute_list, bin_num):
+        print(a, bn)
+        array = np.array(myData[a])
+        bins = [myData[a].min()+(myData[a].max()-myData[a].min())*1.0*i/10 for i in range(11)]
+        plt.title('a')
+        plt.xlabel('value')
+        plt.ylabel('frequency')
+        plt.savefig(save_path+a+'.png')
 
 
 def plotCor(myData, attributeList, save_path):
@@ -31,7 +40,7 @@ def plotCor(myData, attributeList, save_path):
 def main():
     args = getArguments()
     myData = pd.read_csv(args.f, sep=',', encoding='latin1')
-    plotHist(myData, args.hvar, SAVE_PATH)
+    plotHist(myData, args.hvar, args.bins, SAVE_PATH)
     plotCor(myData, args.qvar, SAVE_PATH)
 
 
