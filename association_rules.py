74090b0a9f41422388e92832c7765b8c65b3f03c
Zheyi Wang <36946721+wineinpink@users.noreply.github.com>
2018-11-02 18:08:58 -0400

    Update association_rules.py

--- association_rules.py
+++ association_rules.py
@@ -12,7 +12,7 @@ def getArguments():
     # get and parse command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', type=str, default='./dataset/crime2017_cleaned.csv', help='the file path')
-    parser.add_argument('-s', type=float, default=0.2, help='Minimum support ratio (must be > 0, default: 0.1).')
+    parser.add_argument('-s', type=float, default=0.1, help='Minimum support ratio (must be > 0, default: 0.1).')
     parser.add_argument('-c', type=float, default=0.5, help='Minimum confidence (default: 0.5).')
     return parser.parse_args()
 
