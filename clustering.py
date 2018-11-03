22bea24a42694197f18bd1f4af47213a2b69bfbb
rivenseiun <573935954@qq.com>
2018-11-02 21:43:44 -0400

    Add files via upload

--- clustering.py
+++ clustering.py
@@ -52,38 +52,14 @@ def KmeansClustering():
     print("For n_clusters =", k, "The average calinski_harabaz_score is :", calinski_avg)
     print('\n')
   
-"""
     #plot PCA 
+    normalizedDataFrame["labels"] = cluster_labels
     X = normalizedDataFrame.values
-    y = cluster_labels
-    pca = decomposition.PCA(n_components=3)
-    X = pca.fit_transform(X)
-    
-    fig = plt.figure(1, figsize=(4, 3))
-    plt.clf()
-    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-    plt.cla()
+    pca = decomposition.PCA(n_components=2)
+    plot_columns = pca.fit_transform(normalizedDataFrame)
+    plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=cluster_labels)
+    plt.show()
 
-    for label in [0,1,2,3,4]:
-        ax.text3D(X[y == label, 0].mean(),
-              X[y == label, 1].mean() + 1.5,
-              X[y == label, 2].mean(), label,
-              horizontalalignment='center',
-              bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
-    # Reorder the labels to have colors matching the cluster results
-    y = np.choose(y, [1, 2, 0]).astype(np.float)
-    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.nipy_spectral,
-           edgecolor='k')
-
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-
-plt.show()
-    
-    
-  """  
-    
     
 
 #Agglomerative Clustering
@@ -102,6 +78,14 @@ def Agglomerative():
     print("For n_clusters =", k, "The average calinski_harabaz_score is :", calinski_avg)
     print('\n')
     
+     #plot PCA 
+    normalizedDataFrame["labels"] = cluster_labels
+    X = normalizedDataFrame.values
+    pca = decomposition.PCA(n_components=2)
+    plot_columns = pca.fit_transform(normalizedDataFrame)
+    plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=cluster_labels)
+    plt.show()
+
 
 #DBScan
 def DBScan():
@@ -116,6 +100,15 @@ def DBScan():
     calinski_avg =calinski_harabaz_score(normalizedDataFrame, cluster_labels)
     print("The average calinski_harabaz_score is :", calinski_avg)
     print('\n')
+    
+     #plot PCA 
+    normalizedDataFrame["labels"] = cluster_labels
+    X = normalizedDataFrame.values
+    pca = decomposition.PCA(n_components=2)
+    plot_columns = pca.fit_transform(normalizedDataFrame)
+    plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=cluster_labels)
+    plt.show()
+
 
 
 
